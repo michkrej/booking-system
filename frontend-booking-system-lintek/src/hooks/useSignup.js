08490f9188f323003861b2aff9100e4330512cac
Michelle Krejci <michkrej@gmail.com>
2022-01-30 18:30:33 +0100

    Fixed issue with action

--- frontend-booking-system-lintek/src/hooks/useSignup.js
+++ frontend-booking-system-lintek/src/hooks/useSignup.js
@@ -2,36 +2,33 @@ import { useState } from 'react'
 import { auth, firestore } from '../firebase/config'
 
 const useSignup = () => {
-    const [isPending, setIsPending] = useState(false)
-    const [error, setError] = useState()
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState()
 
-    const signup = async (email, password, displayName, commitee) => {
-        setError(undefined)
-        setIsPending(true)
-        try {
-            const res = await auth.createUserWithEmailAndPassword(
-                email,
-                password
-            )
-            console.log(res.user)
+  const signup = async (email, password, displayName, commitee) => {
+    setError(undefined)
+    setIsPending(true)
+    try {
+      const res = await auth.createUserWithEmailAndPassword(email, password)
+      console.log(res.user)
 
-            if (!res) {
-                throw new Error('Could not create user')
-            }
+      if (!res) {
+        throw new Error('Could not create user')
+      }
 
-            await res.user.updateProfile({ displayName })
-            await firestore.collection('userDetails').add({
-                commitee,
-                userId: res.user.uid,
-            })
-        } catch (error) {
-            console.log(error.message)
-            setError(error.message)
-            setIsPending(false)
-        }
+      await res.user.updateProfile({ displayName })
+      await firestore.collection('userDetails').add({
+        commitee,
+        userId: res.user.uid
+      })
+    } catch (error) {
+      console.log(error.message)
+      setError(error.message)
+      setIsPending(false)
     }
+  }
 
-    return { error, isPending, signup }
+  return { error, isPending, signup }
 }
 
 export default useSignup
--- frontend-booking-system-lintek/src/utils/data.js
+++ frontend-booking-system-lintek/src/utils/data.js
@@ -1,122 +1,122 @@
 const colors = {
-    'C-huset': '#8ecd3c',
-    Utomhus: '#8E44AD',
-    'A-huset': '#2ecc71',
-    Studenthuset: '#f39c12',
-    'B-huset': '#ffd15c',
-    Hemklassrum: '#e9e7fc',
-    'Key-huset': '#00a86b',
-    Kårallen: '#26619c',
-    Annat: '#74867c',
+  'C-huset': '#8ecd3c',
+  Utomhus: '#8E44AD',
+  'A-huset': '#2ecc71',
+  Studenthuset: '#f39c12',
+  'B-huset': '#ffd15c',
+  Hemklassrum: '#e9e7fc',
+  'Key-huset': '#00a86b',
+  Kårallen: '#26619c',
+  Annat: '#74867c'
 }
 
 export const data = [
-    {
-        text: 'Testing',
-        assigneeId: 1,
-        roomId: 1,
-        locationId: 1,
-        eventId: 1,
-        startDate: new Date('2021-04-26T16:30:00.000Z'),
-        endDate: new Date('2021-04-26T20:30:00.000Z'),
-    },
+  {
+    text: 'Testing',
+    assigneeId: 1,
+    roomId: 1,
+    locationId: 1,
+    eventId: 1,
+    startDate: new Date('2021-04-26T16:30:00.000Z'),
+    endDate: new Date('2021-04-26T20:30:00.000Z')
+  }
 ]
 
 export const assignees = [
-    {
-        text: 'STABEN',
-        id: 1,
-        color: '#727bd2',
-    },
-    {
-        text: 'URF',
-        id: 2,
-        color: '#32c9ed',
-    },
-    {
-        text: 'YF',
-        id: 3,
-        color: '#2a7ee4',
-    },
-    {
-        text: 'MPiRE',
-        id: 4,
-        color: '#7b49d3',
-    },
+  {
+    text: 'STABEN',
+    id: 1,
+    color: '#727bd2'
+  },
+  {
+    text: 'URF',
+    id: 2,
+    color: '#32c9ed'
+  },
+  {
+    text: 'YF',
+    id: 3,
+    color: '#2a7ee4'
+  },
+  {
+    text: 'MPiRE',
+    id: 4,
+    color: '#7b49d3'
+  }
 ]
 
 export const locations = [
-    {
-        text: 'C-huset',
-        id: 1,
-        color: colors['C-huset'],
-        value: 1,
-        label: 'C-huset',
-    },
-    {
-        text: 'Utomhus',
-        id: 2,
-        color: colors.Utomhus,
-        value: 2,
-        label: 'Utomhus',
-    },
-    {
-        text: 'A-huset',
-        id: 3,
-        color: colors['A-huset'],
-        value: 3,
-        label: 'A-huset',
-    },
-    {
-        text: 'Key-huset',
-        id: 4,
-        color: colors['Key-huset'],
-        value: 4,
-        label: 'Key-huset',
-    },
+  {
+    text: 'C-huset',
+    id: 1,
+    color: colors['C-huset'],
+    value: 1,
+    label: 'C-huset'
+  },
+  {
+    text: 'Utomhus',
+    id: 2,
+    color: colors.Utomhus,
+    value: 2,
+    label: 'Utomhus'
+  },
+  {
+    text: 'A-huset',
+    id: 3,
+    color: colors['A-huset'],
+    value: 3,
+    label: 'A-huset'
+  },
+  {
+    text: 'Key-huset',
+    id: 4,
+    color: colors['Key-huset'],
+    value: 4,
+    label: 'Key-huset'
+  }
 ]
 
 export const rooms = [
-    {
-        text: 'U1',
-        id: 1,
-        locationId: 1,
-        color: colors['C-huset'],
-    },
-    {
-        text: 'U3',
-        id: 2,
-        locationId: 1,
-        color: colors['C-huset'],
-    },
-    {
-        text: 'R44',
-        id: 3,
-        locationId: 3,
-        color: colors['Key-huset'],
-    },
-    {
-        text: 'R14',
-        id: 4,
-        locationId: 4,
-        color: colors['C-huset'],
-    },
-    {
-        text: 'S-korridoren',
-        id: 5,
-        locationId: 1,
-        color: colors['C-huset'],
-    },
-    {
-        text: 'A1',
-        id: 6,
-        locationId: 2,
-        color: colors.Utomhus,
-    },
-    {
-        text: 'A2',
-        id: 7,
-        locationId: 2,
-        color: colors.Utomhus,
-    },
+  {
+    text: 'U1',
+    id: 1,
+    locationId: 1,
+    color: colors['C-huset']
+  },
+  {
+    text: 'U3',
+    id: 2,
+    locationId: 1,
+    color: colors['C-huset']
+  },
+  {
+    text: 'R44',
+    id: 3,
+    locationId: 3,
+    color: colors['Key-huset']
+  },
+  {
+    text: 'R14',
+    id: 4,
+    locationId: 4,
+    color: colors['C-huset']
+  },
+  {
+    text: 'S-korridoren',
+    id: 5,
+    locationId: 1,
+    color: colors['C-huset']
+  },
+  {
+    text: 'A1',
+    id: 6,
+    locationId: 2,
+    color: colors.Utomhus
+  },
+  {
+    text: 'A2',
+    id: 7,
+    locationId: 2,
+    color: colors.Utomhus
+  }
 ]
