50f67c2eac3e34613c59a22bfdfb04e10e2037d7
Michelle Krejci <mickr592@student.liu.se>
2022-03-12 13:44:56 +0100

    Added öret as room

--- src/utils/campusValla.js
+++ src/utils/campusValla.js
@@ -47,12 +47,6 @@ export const locationsValla = {
     text: 'B-huset',
     label: 'B-huset'
   },
-  Kårhus: {
-    id: 'de6ebd1a-eb0b-4de4-b549-bc57bb9fadf5',
-    value: 'de6ebd1a-eb0b-4de4-b549-bc57bb9fadf5',
-    text: 'Kårhus',
-    label: 'Kårhus'
-  },
   'Övriga områden på campus': {
     id: '3b651c71-3202-47e8-8b94-d5ba7175b1d0',
     value: '3b651c71-3202-47e8-8b94-d5ba7175b1d0',
@@ -1013,59 +1007,6 @@ const roomsB = [
   }
 ]
 
-const karhus = [
-  {
-    text: 'HG',
-    id: '07f88233-1f48-4781-8d89-2669a80e393d',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'KK - stora salen',
-    id: 'c510648d-37a0-4019-87e1-fbbf1b1adf3a',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'KK - lilla salen',
-    id: 'd1d12a5b-8bcb-47b2-9450-e0d490aa6a96',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'KK',
-    id: 'a232bbef-b3c3-4e0b-9736-3775139578d9',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Kårallen - matsalen',
-    id: 'fa5bc487-5753-48da-beb7-bf6b31b9f94a',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Kårallen - gasquen',
-    id: 'a2f3bf6e-f7a1-4e7a-8c8f-9119a147f558',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Kårallen - baljan',
-    id: '50923589-f79b-4770-ac8b-35fee2a497f2',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Kårallen',
-    id: 'ed559a94-faaf-4daf-b70f-c7439fb989d7',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Amfi',
-    id: 'bc15ae77-59f4-4531-9790-6040f264bb87',
-    locationId: locationsValla['Kårhus'].id
-  },
-  {
-    text: 'Blå havet',
-    id: '5c7326f1-b3a9-4e62-9a25-923a97cb181f',
-    locationId: locationsValla['Kårhus'].id
-  }
-]
-
 const outsideCampus = [
   {
     text: 'Område 1',
@@ -1170,7 +1111,6 @@ export const roomsValla = [
   ...roomsI,
   ...roomsKey,
   ...roomsStudenthuset,
-  ...karhus,
   ...outsideCampus,
   ...otherCampus
 ]
