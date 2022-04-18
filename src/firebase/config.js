9b4a5dc205702c19384759286bf9ceb3e493c9fd
Michelle Krejci <mickr592@student.liu.se>
2022-04-18 17:35:30 +0200

    Working on export function

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_ANALYTICS_ID
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 firebase.initializeApp(firebaseConfig)
