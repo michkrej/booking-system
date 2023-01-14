956bb2633873c8ab6dfa3e77a9d27d803c558506
Michelle Krejci <michkrej@gmail.com>
2023-01-14 15:40:24 +0100

    Added develop db & fixed some errors that occured when plans was empty

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,22 +7,30 @@ import 'firebase/app-check'
 
 if (window.location.hostname == 'localhost') self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_ANALYTICS_ID
-}
+const firebaseConfig =
+  window.location.hostname == 'localhost'
+    ? {
+        apiKey: process.env.REACT_APP_DEV_API,
+        authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_DEV_APP_ID
+      }
+    : {
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+        measurementId: process.env.REACT_APP_FIREBASE_ANALYTICS_ID
+      }
 
 firebase.initializeApp(firebaseConfig)
 
 export const analytics = firebase.analytics()
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
-const appCheck = firebase.appCheck()
-appCheck.activate('6LdGo4IfAAAAAOD_vLtVHrABhtu40ntNZZKyFXm7', true)
 
 export default firebase
