cb94553bc3321fa26f611b80b219338f6ef23fef
Michelle Krejci <michkrej@gmail.com>
2023-06-04 19:07:38 +0200

    Added strict mode

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import AuthContextProvider from './context/AuthContextProvider'
 import PlansContextProvider from './context/PlanContextProvider'
+import { StrictMode } from 'react'
 
 const container = document.getElementById('root')
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
 root.render(
-  <AuthContextProvider>
-    <PlansContextProvider>
-      <App />
-    </PlansContextProvider>
-  </AuthContextProvider>
+  <StrictMode>
+    <AuthContextProvider>
+      <PlansContextProvider>
+        <App />
+      </PlansContextProvider>
+    </AuthContextProvider>
+  </StrictMode>
 )
