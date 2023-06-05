6151fcbed80fbe686fff6d141522369d53699129
Michelle Krejci <michkrej@gmail.com>
2023-06-05 11:56:07 +0200

    Extracted routing into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { lazy, Suspense, useEffect } from 'react'
+import { useEffect } from 'react'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { Container, CssBaseline, Box, CircularProgress } from '@mui/material'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Container, CssBaseline, Box } from '@mui/material'
 import { locale, loadMessages } from 'devextreme/localization'
 
 import Footer from './components/Footer'
-import Login from './pages/Login'
-import RequireAuth from './components/RequireAuth'
-import Nav from './components/Nav'
 import useAuthContext from './hooks/context/useAuthContext'
 
 import seMessages from 'devextreme/localization/messages/sv.json'
 import 'devextreme/dist/css/dx.light.css'
-
-const Overview = lazy(() => import('./pages/Overview'))
-const Signup = lazy(() => import('./pages/Signup'))
-const CalendarView = lazy(() => import('./pages/CalendarView'))
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+import Router from './Router'
 
 export const primary = '#670c47'
 export const secondary = '#bcdfd7'
@@ -34,7 +26,7 @@ export const theme = createTheme({
 })
 
 function App() {
-  const { authFinished, user } = useAuthContext()
+  const { authFinished } = useAuthContext()
 
   useEffect(() => {
     loadMessages(seMessages)
@@ -53,57 +45,7 @@ function App() {
       >
         <CssBaseline />
         <Container component="main" pb={5} sx={{ flexGrow: 1 }} maxWidth="xxl">
-          {authFinished && (
-            <BrowserRouter>
-              <Suspense
-                fallback={
-                  <>
-                    <Nav />
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh'
-                      }}
-                    >
-                      <CircularProgress />
-                    </Box>
-                    <Footer />
-                  </>
-                }
-              >
-                <Routes>
-                  <Route exact path="/" element={user ? <Navigate to="/overview" /> : <Login />} />
-                  <Route
-                    exact
-                    path="/signup"
-                    element={user ? <Navigate to="/overview" /> : <Signup />}
-                  />
-                  <Route exact path="/resetPassword" element={<ForgotPassword />} />
-                  <Route element={<RequireAuth />}>
-                    <Route exact path="/booking/:id" element={<CalendarView />} />
-                    <Route exact path="/collisions/:id" element={<CalendarView findCollisions />} />
-                    <Route
-                      exact
-                      path="/collisions/all/:id"
-                      element={<CalendarView findCollisions />}
-                    />
-                    <Route exact path="/overview" element={<Overview />} />
-                    <Route exact path="/allEvents/:id" element={<CalendarView showAllEvents />} />
-                  </Route>
-                  <Route
-                    path="*"
-                    element={
-                      <main style={{ padding: '1rem' }}>
-                        <p>Oops, denna sidan är tom!</p>
-                      </main>
-                    }
-                  />
-                </Routes>
-              </Suspense>
-            </BrowserRouter>
-          )}
+          {authFinished && <Router />}
         </Container>
         <Footer />
       </Box>
---
+++ src/Router.jsx
@@ -0,0 +1,66 @@
+import { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Box, CircularProgress } from '@mui/material'
+
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import useAuthContext from './hooks/context/useAuthContext'
+import RequireAuth from './components/RequireAuth'
+import Login from './pages/Login'
+
+const Overview = lazy(() => import('./pages/Overview'))
+const Signup = lazy(() => import('./pages/Signup'))
+const CalendarView = lazy(() => import('./pages/CalendarView'))
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+
+const Fallback = () => {
+  return (
+    <>
+      <Nav />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+        }}
+      >
+        <CircularProgress />
+      </Box>
+      <Footer />
+    </>
+  )
+}
+
+const Router = () => {
+  const { user } = useAuthContext()
+
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Fallback />}>
+        <Routes>
+          <Route exact path="/" element={user ? <Navigate to="/overview" /> : <Login />} />
+          <Route exact path="/signup" element={user ? <Navigate to="/overview" /> : <Signup />} />
+          <Route exact path="/resetPassword" element={<ForgotPassword />} />
+          <Route element={<RequireAuth />}>
+            <Route exact path="/booking/:id" element={<CalendarView />} />
+            <Route exact path="/collisions/:id" element={<CalendarView findCollisions />} />
+            <Route exact path="/collisions/all/:id" element={<CalendarView findCollisions />} />
+            <Route exact path="/overview" element={<Overview />} />
+            <Route exact path="/allEvents/:id" element={<CalendarView showAllEvents />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Oops, denna sidan är tom!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+export default Router
