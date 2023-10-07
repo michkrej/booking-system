289ff1182bf20096c2aca9954d644442dccf66ab
Michelle Krejci <michkrej@gmail.com>
2023-10-07 16:24:46 +0200

    refactor: improved firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,25 +4,15 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getAnalytics } from 'firebase/analytics'
 
-const firebaseConfig =
-  window.location.hostname == 'localhost'
-    ? {
-        apiKey: process.env.REACT_APP_DEV_API,
-        authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_DEV_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_DEV_APP_ID
-      }
-    : {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-        appId: process.env.REACT_APP_FIREBASE_APP_ID,
-        measurementId: process.env.REACT_APP_FIREBASE_ANALYTICS_ID
-      }
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_ANALYTICS_ID
+}
 
 const app = initializeApp(firebaseConfig)
 
