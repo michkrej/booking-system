908b86577c1b2c5d3c99bfb2094d88d718d00ccf
Michelle Krejci <michkrej@gmail.com>
2025-03-16 11:52:02 +0100

    build: fix build error because of faulty vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,26 +10,26 @@ export default defineConfig({
     react(),
     viteTsconfigPaths(),
     svgr({
-      include: '**/*.svg?react'
-    })
+      include: "**/*.svg?react",
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@states': path.resolve(__dirname, './src/state'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@data': path.resolve(__dirname, './src/data'),
-      process: 'process/browser',
-      util: 'util'
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@states": path.resolve(__dirname, "./src/state"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      process: "process/browser",
+      util: "util",
+    },
   },
   server: {
     port: 4000,
-    open: true
+    open: true,
   },
-  base: './'
-})
+  //base: './'
+});
