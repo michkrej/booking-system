f0477196cde2e95af2a53afbbe805bdce046d1c8
Michelle Krejci <michkrej@gmail.com>
2025-04-06 19:21:03 +0200

    fix: improve date validation for start and end date for bookings

--- src/pages/Booking/components/schema.ts
+++ src/pages/Booking/components/schema.ts
@@ -1,31 +1,40 @@
 import { z } from "zod";
 
-export const BookingSchema = z.object({
-  title: z.string().min(1, "Bokningen måste ha ett namn"),
-  description: z.string().optional().default(""),
-  startDate: z.date(),
-  endDate: z.date(),
-  rooms: z
-    .array(z.object({ value: z.string(), label: z.string() }))
-    .min(1, "Bokningen måste ha minst en rum"),
-  food: z.boolean(),
-  alcohol: z.boolean(),
-  link: z.string(),
-  bookableItems: z.array(
-    z.object({
-      key: z.string(),
-      value: z
-        .string()
-        .min(1, {
-          message: "Fältet är obligatoriskt",
-        })
-        .or(
-          z.number().min(1, {
+export const BookingSchema = z
+  .object({
+    title: z.string().min(1, "Bokningen måste ha ett namn"),
+    description: z.string().optional().default(""),
+    startDate: z.date(),
+    endDate: z.date(),
+    rooms: z
+      .array(z.object({ value: z.string(), label: z.string() }))
+      .min(1, "Bokningen måste ha minst en rum"),
+    food: z.boolean(),
+    alcohol: z.boolean(),
+    link: z.string(),
+    bookableItems: z.array(
+      z.object({
+        key: z.string(),
+        value: z
+          .string()
+          .min(1, {
             message: "Fältet är obligatoriskt",
-          }),
-        ),
-      startDate: z.coerce.date(),
-      endDate: z.coerce.date(),
-    }),
-  ),
-});
+          })
+          .or(
+            z.number().min(1, {
+              message: "Fältet är obligatoriskt",
+            }),
+          ),
+        startDate: z.coerce.date(),
+        endDate: z.coerce.date(),
+      }),
+    ),
+  })
+  .superRefine((booking, ctx) => {
+    if (booking.startDate > booking.endDate) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Startdatum måste vara före slutdatum",
+      });
+    }
+  });
